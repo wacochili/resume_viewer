874804a75c212a14c6764acaf83b6ec03720c71d
Natasha Wu <natasha.wu@gmail.com>
2015-07-07 19:57:16 -0500

    pulling data from API in show page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,10 @@
 class StudentsController < ApplicationController
   def show
+    @student = Student.find(params[:id])
     
+    # @student = Unirest.post("http://localhost:3000/students.json", headers:{ "Accept" => "application/json" }, parameters:{ first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number], short_bio: params[:short_bio], linkedin: params[:linkedin], twitter: params[:twitter], blog_site: params[:blog_site], resume: params[:resume], github: params[:github], }).body
+
+    # redirect_to "/students/#{@student['id']}"
   end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,24 @@
 class Student
+  attr_accessor :id, :first_name, :last_name, :email, :phone_number, :short_bio, :linkedin, :twitter, :blog_site, :resume, :github, :photo
+
+  def initialize(hash)
+    @id = hash["id"]
+    @first_name = hash["first_name"]
+    @last_name = hash["last_name"]
+    @email = hash["email"]
+    @phone_number = hash["phone_number"]
+    @short_bio = hash["short_bio"]
+    @linkedin = hash["linkedin"]
+    @twitter = hash["twitter"]
+    @blog_site = hash["blog_site"]
+    @resume = hash["resume"]
+    @github = hash["github"]
+    @photo = hash["photo"]
+  end
+
+  def self.find(id)
+    student_hash = Unirest.get("http://localhost:3000/students/#{id}.json").body
+    Student.new(student_hash)
+  end
 
 end
